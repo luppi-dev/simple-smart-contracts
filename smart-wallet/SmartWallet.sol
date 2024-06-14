5a505eb0ec11dd12173a7a88c036169c5949e979
Gabriel Luppi <luppidev.gg@gmail.com>
2024-06-14 00:16:09 -0300

    <feat>(Guardian): Add guardians operation for propose new wallet owner

--- smart-wallet/SmartWallet.sol
+++ smart-wallet/SmartWallet.sol
@@ -18,12 +18,41 @@ contract SmartWallet {
     mapping(address => uint) public allowance;
     mapping(address => bool) public isAllowedToSend;
 
+    mapping(address => bool) public guardians;
+    address payable nextOwner;
+    mapping(address => mapping(address => bool)) guardianAlreadyVoted;
+    uint guardiansResetCount;
+    uint public constant guardianConfirmationsForReset = 3;
+
+
     constructor() {
         owner = payable(msg.sender); // first to use the contract is defined as the owner
     }
+    
+
+    function setGuardian(address _guardian, bool _isGuardian) public {
+        onlyOwnerOperation();
+        guardians[_guardian] = _isGuardian;
+    }
+
+    function proposeNewOwner(address payable _newOwner) public {
+        require(guardians[msg.sender], "You are not a guardian.");
+        require(guardianAlreadyVoted[_newOwner][msg.sender] == false, "You already voted.");
+        if(_newOwner != nextOwner) {
+            nextOwner = _newOwner;
+            guardiansResetCount = 0;
+        }
+
+        guardiansResetCount++;
+
+        if(guardiansResetCount >= guardianConfirmationsForReset) {
+            owner = nextOwner;
+            nextOwner = payable(address(0));
+        }
+    }
 
     function setAllowance(address _for, uint _amount) public {
-        require(msg.sender == owner, "Only the contract owner can access this operation.");
+        onlyOwnerOperation();
         allowance[msg.sender] = _amount;
 
         if(_amount > 0) {
@@ -45,6 +74,10 @@ contract SmartWallet {
         (bool success, bytes memory data) = _to.call{value: _amount}(_payload);
         require(success, "Call was not successful");
         return data;
+    }
+    
+    function onlyOwnerOperation() private view {
+        require(msg.sender == owner, "Only the guardian owner can access this operation.");
     }
 
     receive() external payable {} // receives ETH, can only be accessed only outside of this contract
